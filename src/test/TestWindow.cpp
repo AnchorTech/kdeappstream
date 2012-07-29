50c236e9a0ad080da3eeedc8c51e7bbb4e4908be
Łukasz Spas <lukasz.spas@gmail.com>
2012-07-30 00:10:50 +0200

    test app mod

--- src/test/TestWindow.cpp
+++ src/test/TestWindow.cpp
@@ -8,20 +8,20 @@ TestWindow::TestWindow(QWidget *parent) :
     ui(new Ui::TestWindow)
 {
     ui->setupUi(this);
-    QGraphicsScene * scene = new QGraphicsScene(0, 0, 1000, 1000);
-    ui->graphicsView->setScene(scene);
-    ui->graphicsView->scene()->addEllipse(50, 50, 50, 25, QPen(Qt::red), QBrush(Qt::blue));
-    ui->graphicsView->scene()->addEllipse(50, 65, 50, 25, QPen(Qt::red));
-    ui->graphicsView->scene()->addLine(50, 50, 50, 25, QPen(Qt::green));
-    ui->graphicsView->scene()->addEllipse(50, 65, 50, 25, QPen(Qt::red));
+//    QGraphicsScene * scene = new QGraphicsScene(0, 0, 1000, 1000);
+//    ui->graphicsView->setScene(scene);
+//    ui->graphicsView->scene()->addEllipse(50, 50, 50, 25, QPen(Qt::red), QBrush(Qt::blue));
+//    ui->graphicsView->scene()->addEllipse(50, 65, 50, 25, QPen(Qt::red));
+//    ui->graphicsView->scene()->addLine(50, 50, 50, 25, QPen(Qt::green));
+//    ui->graphicsView->scene()->addEllipse(50, 65, 50, 25, QPen(Qt::red));
 
-    QPainterPath path;
-    path.moveTo(0,0);
-    path.lineTo(10,10);
-    path.addText(100, 100, QFont("Sans Serif", 100, 50, true), "Some text");
-    path.arcMoveTo(200, 200, 100, 100, 50);
-    path.cubicTo(500, 500, 300, 300, 100, 100);
-    ui->graphicsView->scene()->addPath(path, QPen(Qt::gray, 10, Qt::DashDotLine, Qt::RoundCap, Qt::MiterJoin));
+//    QPainterPath path;
+//    path.moveTo(0,0);
+//    path.lineTo(10,10);
+//    path.addText(100, 100, QFont("Sans Serif", 100, 50, true), "Some text");
+//    path.arcMoveTo(200, 200, 100, 100, 50);
+//    path.cubicTo(500, 500, 300, 300, 100, 100);
+//    ui->graphicsView->scene()->addPath(path, QPen(Qt::gray, 10, Qt::DashDotLine, Qt::RoundCap, Qt::MiterJoin));
 }
 
 TestWindow::~TestWindow()
